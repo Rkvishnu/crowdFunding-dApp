c2a9a831d9bef17a35acbe95a25a5e8e66634c44
Vishnu Rathore <vr8043836@gamil.com>
2022-07-15 23:05:24 +0530

    infinite

--- test.js
+++ test.js
@@ -4,33 +4,32 @@ const { ethers } = require("ethers");
 require("dotenv").config({ path: "./.env.local" });
 
 const main = async () => {
-  
-    // const provider = new ethers.providers.JsonRpcProvider(
-    //   process.env.NEXT_PUBLIC_RPC_URL
-    // );
+  //   const provider = new ethers.providers.JsonRpcProvider(
+  //     process.env.NEXT_PUBLIC_RPC_URL
+  //   );
 
-    // const contract = new ethers.Contract(
-    //   process.env.NEXT_PUBLIC_ADDRESS,
-    //   CampaignFactory.abi,
-    //   provider
-    // );
+  //   const contract = new ethers.Contract(
+  //     process.env.NEXT_PUBLIC_ADDRESS,
+  //     CampaignFactory.abi,
+  //     provider
+  //   );
 
-    // const getDeployedCampaign = contract.filters.campaignCreated(null,null,null,null,null,null,'Health');
-    // let events = await contract.queryFilter(getDeployedCampaign);
-    // let event = events.reverse();
-    // console.log(event);
+  //   const getDeployedCampaign = contract.filters.campaignCreated(null,null,null,null,null,null,'Health');
+  //   let events = await contract.queryFilter(getDeployedCampaign);
+  //   let event = events.reverse();
+  //   console.log(event);
 
   const provider = new ethers.providers.JsonRpcProvider(
-   "https://polygon-mumbai.g.alchemy.com/v2/Me5PfrxFniiVboBMmPfoHpfyYqBS2GLf"
+    process.env.NEXT_PUBLIC_RPC_URL
   );
 
   const contract = new ethers.Contract(
-    "0x9Cd0f313d9FE6556A15d6d44CE64dd1FD7318e73",
+    "0xE9Ad3074Fe9c5fD89E30d02046d5A49a9A469421",
     Campaign.abi,
     provider
   );
 
-  const Donations = contract.filters.donated('0xE9Ad3074Fe9c5fD89E30d02046d5A49a9A469421');
+  const Donations = contract.filters.donated('0xc3Ee94A5Dd1902f7c5da78a8553010fAa5C081a0');
   const AllDonations = await contract.queryFilter(Donations);
 
   const DonationsData =  AllDonations.map((e) => {
