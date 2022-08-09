7286af31df0c08b86ec9e6f25ad6361638916277
Vishnu Rathore <vr8043836@gamil.com>
2022-08-09 19:39:38 +0530

    update5

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.getAddress());
   }
 })
 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,18 +1,20 @@
-const hre = require('hardhat');
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+// const { CRYPTO_DEV_TOKEN_CONTRACT_ADDRESS } = require("../constants/index");
 
 async function main() {
-// getcontractfactory is provide by ethers js from that we can access our contracts from artifacts
-    const CampaignFactory = await hre.ethers.getContractFactory("CampaignFactory")
+    const CampaignFactory = await ethers.getContractFactory("CampaignFactory")
     const campaignFactory = await CampaignFactory.deploy();
 
     await campaignFactory.deployed();
 
     console.log("Factory deployed to:", campaignFactory.address);
-}   
+}
 
+// Call the main function and catch if there is any error
 main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-        console.log(error);
-        process.exit(1);
-    });
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
